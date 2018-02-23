88ebe84f2fc72900965634a268ebcdc466e096c8
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-02-23 08:11:10 +0100

    pull.sh: rename orgmode to org-mode

--- pull.sh
+++ pull.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tobepull="bash-examples c-examples python-examples davidam.github.io libremanuals.github.io worg orgmode"
+tobepull="bash-examples c-examples python-examples davidam.github.io libremanuals.github.io worg org-mode"
 gitdir="/home/davidam/git"
 cd $gitdir
 for i in $tobepull; do
