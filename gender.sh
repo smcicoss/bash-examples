b407bba12f371f7a55df2c98a12ecd4c1cd7dffb
David Arroyo <davidam@gmail.com>
2018-06-02 10:04:54 +0200

    gender.sh: take 2 gender into account

--- gender.sh
+++ gender.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
+# Copyright (C) 2018  David Arroyo Menéndez
 
-gender="female"
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
 
-if [[ "$gender" == f* ]]
-then echo "Pleasure to meet you, Madame."; fi
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+# Boston, MA 02110-1301 USA,
+
+echo "Give me your name:"
+read name
+
+if grep -i --quiet $name masculinos.txt; then
+  echo "You are a male"
+fi
+
+if grep -i --quiet $name femeninos.txt; then
+  echo "You are a female"
+fi
+
