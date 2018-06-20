a0815981f9d81d9eeb70281d864cc0c4009ef97e
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-06-20 10:54:42 +0200

    pull.sh: extend tobepull

--- pull.sh
+++ pull.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-tobepull="bash-examples c-examples python-examples davidam.github.io libremanuals.github.io worg org-mode"
+tobepull="bash-examples bashrefes c-examples davidam davidam.github.io haskell-examples java-examples js-examples gnuplot-examples marenostrum-examples php-mode ruby-examples sql-examples libremanuals.github.io worg org-mode orgguide-es GAPLEN python-examples"
+
 gitdir="/home/davidam/git"
 cd $gitdir
 for i in $tobepull; do
