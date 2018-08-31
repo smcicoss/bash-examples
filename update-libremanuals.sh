9db76b59a6e4749f6f7f560991e66e92bafa324e
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-08-31 10:49:44 +0200

    update-libremanuals.sh: sed and wget with array and for

--- update-libremanuals.sh
+++ update-libremanuals.sh
@@ -24,28 +24,14 @@
 
 cd ~/git/libremanuals.github.io/download
 rm *
-wget http://127.0.0.1:5000/index.html
-wget http://127.0.0.1:5000/elisp-es.html
-wget http://127.0.0.1:5000/org-mode.html
-wget http://127.0.0.1:5000/gcc.html
-wget http://127.0.0.1:5000/r.html
-wget http://127.0.0.1:5000/python.html
-wget http://127.0.0.1:5000/bash.html
-wget http://127.0.0.1:5000/debian.html
-wget http://127.0.0.1:5000/gimp.html
-wget http://127.0.0.1:5000/acerca.html
 
-sed -i "s/\/static\///g" index.html
-sed -i "s/\/static\///g" elisp-es.html
-sed -i "s/\/static\///g" org-mode.html
-sed -i "s/\/static\///g" gcc.html
-sed -i "s/\/static\///g" r.html
-# sed -i "s/static\/vendor\/bootstrap\///g" python.html
-sed -i "s/\/static\///g" python.html
-sed -i "s/\/static\///g" bash.html
-sed -i "s/\/static\///g" debian.html
-sed -i "s/\/static\///g" gimp.html
-sed -i "s/\/static\///g" acerca.html
+array=(index elisp-es org-mode gcc r python bash debian gimp acerca)
+
+for i in ${array[*]}
+do
+	wget http://127.0.0.1:5000/$i.html
+	sed -i "s/\/static\///g" $i.html
+done
 
 cp * ..
 cd ..
