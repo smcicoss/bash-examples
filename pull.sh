201043657fd54423f7ed894a9abfbfa7d520d76c
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-03-27 03:42:25 +0100

    clone.sh, pull.sh: new repository

--- pull.sh
+++ pull.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tobepull="bash-examples bashrefes c-examples davidam davidam.github.io haskell-examples java-examples js-examples gnuplot-examples marenostrum-examples php-mode ruby-examples sql-examples libremanuals.github.io worg org-mode orgguide-es GAPLEN python-examples"
+tobepull="bash-examples bashrefes c-examples davidam davidam.github.io haskell-examples java-examples js-examples gnuplot-examples marenostrum-examples php-mode ruby-examples sql-examples libremanuals.github.io worg org-mode orgguide-es GAPLEN python-examples certificaciones"
 
 gitdir="/home/davidam/git"
 cd $gitdir
