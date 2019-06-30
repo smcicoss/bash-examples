42353364a9e5b14fef6b696ff8a0615354f9e18d
smcicoss <simon@cicoss.net>
2019-07-01 01:38:35 +0200

    separación a otro repositorio de derivado de elimDobles.sh

--- elimDobles.sh
+++ elimDobles.sh
@@ -2,15 +2,24 @@
 # -*- ENCODING: UTF-8 -*-
 # ·
 
+# original de: David Arroyo Menéndez
+# http://www.davidam.com/
+# En: https://github.com/davidam/bash-examples
+
+
+# Simón Martínez Álvarez [simon@cicoss.net]
+
 # Con arboles de directorios grandes la cantidad de memoria 
 # utilizada es enorme ya que hace una lectura previa de todos
 # los nombres de fichero antes de empezar a comparar y el tiempo
 # empleado en esa lectura tambien se vuelve inasumible, así como
 # el tiempo empleado por md5sum en ficheros grandes.
 # Por otro lado no permite elegir cual borrar.
+# Otro problema, grave, que presenta este script es que si dentro
+# del arbol tenemos un enlace simbolico a otra parte del mismo
+# arbol borrará todos los ficheros que existan en esa rama.
 
-declare -A arr
-shopt -s globstar
+################################################################
 
 for file in **; do
   [[ -f "$file" ]] || continue
@@ -18,6 +27,6 @@ for file in **; do
   read cksm _ < <(md5sum "$file")
   if ((arr[$cksm]++)); then
     echo "rm $file"
-    rm $file
+    rm "$file"
   fi
 done
